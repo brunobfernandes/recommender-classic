f03eb3ae51d14325063d2ea62fab21be2774f631
Juarez Sacenti <juarezsacenti@gmail.com>
2017-04-14 14:45:57 -0300

    Add item-based recommender example

--- recommender-mc/src/main/java/lapesd/sacenti/recommender/recommender_mc/App.java
+++ recommender-mc/src/main/java/lapesd/sacenti/recommender/recommender_mc/App.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 /**
@@ -24,9 +28,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        showUserRecommender();
+        showItemRecommender();
     }
     
+    // From tutorial <https://mahout.apache.org/users/recommender/userbased-5-minutes.html>
     public static void showUserRecommender() 
     {
     	DataModel model;
@@ -35,7 +40,7 @@ public class App
     	UserBasedRecommender recommender;
     	
     	try {
-			model = new FileDataModel(new File("./data/simple-dataset.csv"));
+			model = new FileDataModel(new File("data/simple-dataset.csv"));
 	    	similarity = new PearsonCorrelationSimilarity(model);
 	    	neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
 	    	recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
@@ -45,16 +50,45 @@ public class App
 	    	  System.out.println(recommendation);
 	    	}
 	    } catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("There was an IO exception.");
 			e.printStackTrace();
 		} catch (TasteException e) {
-			// TODO Auto-generated catch block
+			System.out.println("There was an Taste exception.");
 			e.printStackTrace();
-		}
+		}    
     }
     
+    // from tutorial <https://www.youtube.com/watch?v=yD40rVKUwPI>
     public static void showItemRecommender() 
     {
-        System.out.println( "In construction!" );
+    	DataModel model;
+    	ItemSimilarity similarity;
+    	GenericItemBasedRecommender recommender;
+    	try {
+			model = new FileDataModel(new File("data/simple-dataset.csv"));
+	    	similarity = new LogLikelihoodSimilarity(model);
+	    	recommender = new GenericItemBasedRecommender(model, similarity);
+	    
+	    	for(LongPrimitiveIterator items = model.getItemIDs(); items.hasNext();) {
+	    		long itemId = items.nextLong();
+	    		
+	    		List<RecommendedItem> recommendations = recommender.mostSimilarItems(itemId, 3);
+	    		for (RecommendedItem recommendation : recommendations) {
+	    			System.out.println(itemId + ", " + recommendation.getItemID() + ", " + recommendation.getValue());
+	    		}
+	    	}
+	    } catch (IOException e) {
+			System.out.println("There was an IO exception.");
+			e.printStackTrace();
+		} catch (TasteException e) {
+			System.out.println("There was an Taste exception.");
+			e.printStackTrace();
+		}    
+    }
+    
+    // From article <https://arxiv.org/ftp/arxiv/papers/1503/1503.06562.pdf>
+    public static void showMultiCriteriaRecommender() 
+    {
+    	System.out.println("Under construction...");
     }
 }
