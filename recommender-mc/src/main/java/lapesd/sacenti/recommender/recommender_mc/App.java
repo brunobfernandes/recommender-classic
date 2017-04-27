cd6ae988a1f7465b3dee4e62228ce53a9f177497
bruno <bruno@DESKTOP-1HQPKLB>
2017-04-27 16:40:12 -0300

    Medidas de avaliação de SR

--- recommender-mc/src/main/java/lapesd/sacenti/recommender/recommender_mc/App.java
+++ recommender-mc/src/main/java/lapesd/sacenti/recommender/recommender_mc/App.java
@@ -4,146 +4,99 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.lang.math.RandomUtils;
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.RMSRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        showUserRecommender();
+    	String etapa0_entrada1 = "data/simple-dataset.csv";
+    	RecomendacaoFiltragemColaborativa(etapa0_entrada1);
     }
     
-    // From tutorial <https://mahout.apache.org/users/recommender/userbased-5-minutes.html>
-    public static void showUserRecommender() 
+    
+    public static void RecomendacaoFiltragemColaborativa(String inputFile) 
     {
-    	/* DataModel = { USER, ITEM, PREFERENCE }
-    	 * then, u in USER and p in PREFERENCE, u = {p1, p2, p3, p4 ,p5, ...}. 
-    	 * 
-    	 * The PearsonCorrelationSimilary define the similarity between user's vector u' and u'',
-    	 * where sim(u', u'') = cos(u' angle u'').
-    	 * 
-    	 * The ThresholdUserNeighborhood of u' is an user group UN, 
-    	 * where foreach u'' in UN, u'' != u' and sim(u', u'') > threshold.
-    	 * 
-    	 * A GenericUserBasedRecommender recommends to an user u' a list of top items TI,
-    	 * where i in TI, u'' in UN(u') | i in u''.items and TI is ordered.
-    	 * */
-    	DataModel model;
+    	/*DataModel model;
     	UserSimilarity similarity;
     	UserNeighborhood neighborhood;
     	UserBasedRecommender recommender;
     	
     	try {
-			model = new FileDataModel(new File("data/simple-dataset.csv"));
+			model = new FileDataModel(new File(entrada));
 	    	similarity = new PearsonCorrelationSimilarity(model);
 	    	neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
 	    	recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-	    
-	    	List<RecommendedItem> recommendations = recommender.recommend(2, 3);
-	    	for (RecommendedItem recommendation : recommendations) {
-	    	  System.out.println(recommendation);
-	    	}
-	    } catch (IOException e) {
-			System.out.println("There was an IO exception.");
-			e.printStackTrace();
-		} catch (TasteException e) {
-			System.out.println("There was an Taste exception.");
-			e.printStackTrace();
-		}    
-    }
-    
-    // from tutorial <https://www.youtube.com/watch?v=yD40rVKUwPI>
-    public static void showItemRecommender() 
-    {
-    	/* DataModel = { USER, ITEM, PREFERENCE }
-    	 * 
-    	 * The LogLikelihoodSimilary define the similarity between items i' and i'',
-    	 * where sim(i', i'') = 1.0 - 1.0 / (1.0 + logLikelihoodRatio) and logLikelihoodRatio = (2.0 * (rowEntropy + columnEntropy - matrixEntropy))
-    	 * 
-    	 * A GenericUserBasedRecommender recommends to an item i' a list of top items TI,
-    	 * where i'' in TI, sim(i', i'') and TI is ordered.
-    	 * i.e.: returns items that have not been rated by the user and that were preferred by another user
-    	 * that has preferred at least one item that the current user has preferred too
-    	 * */
-    	DataModel model;
-    	ItemSimilarity similarity;
-    	GenericItemBasedRecommender recommender;
+		}	
+		*/
+    	//Controi o ambiente para testar as recomendações
+    	RecommenderBuilder userSimRecBuilder = new RecommenderBuilder() {
+    		public Recommender buildRecommender(DataModel model)throws TasteException
+    		{
+    			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+    			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+    	   		Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+    			return recommender;
+    		}
+    	};
+
     	try {
-			model = new FileDataModel(new File("data/simple-dataset.csv"));
-	    	similarity = new LogLikelihoodSimilarity(model);
-	    	recommender = new GenericItemBasedRecommender(model, similarity);
-	    
-	    	for(LongPrimitiveIterator items = model.getItemIDs(); items.hasNext();) {
-	    		long itemId = items.nextLong();
-	    		
-	    		List<RecommendedItem> recommendations = recommender.mostSimilarItems(itemId, 3);
-	    		for (RecommendedItem recommendation : recommendations) {
-	    			System.out.println(itemId + ", " + recommendation.getItemID() + ", " + recommendation.getValue());
-	    		}
-	    	}
-	    } catch (IOException e) {
-			System.out.println("There was an IO exception.");
-			e.printStackTrace();
-		} catch (TasteException e) {
-			System.out.println("There was an Taste exception.");
-			e.printStackTrace();
-		}    
-    }
-    
-    // From article <https://arxiv.org/ftp/arxiv/papers/1503/1503.06562.pdf>
-    public static void showMultiCriteriaRecommender() 
-    {
-    	/* DataModel = { USER, ITEM, {PREFERENCEoverall, PREFERENCEcriteria1, PREFERENCEcriteria2, ...} }
-    	 * ! Foreach u' in USER, u' =  { ITEM1.{PREFERENCEoverall, PREFERENCEcriteria1, PREFERENCEcriteria2, ... },
-    	 * 								 ITEM2.{PREFERENCEoverall, PREFERENCEcriteria1, PREFERENCEcriteria2, ... },
-    	 * 								 ... }
-    	 * 
-    	 * EstimatedOverallPreference is a estimated preference of i' for u' ,
-    	 * where eOverallPreference(i', u') = 
-    	 * 
-    	 * MultiCriteriaRecommender has 2 steps: 1. predicting rating for a particular unseen item;
-    	 * 2. recommending to an user u' a list of top items TI,
-    	 * where i' in TI,  TI is ordered by eOverallPreference(i', u').
-    	 * i.e.: returns items that have not been rated by the user, ordered by estimated preference of that user
-    	 * 
-    	 * Obs.: SVD, SSVD, PCA, HOSVD with map reduce.
-    	 * 
-    	 * */
-    	System.out.println("Under construction...");
-    }
+    		FileDataModel dataModel = new FileDataModel(new File(inputFile));
+    		//(Detalhes da técnica; Padrão; Conjunto de dados;  %preferências por usuários; %usuários) 
+    		
+    		RecommenderEvaluator evaluator = new RMSRecommenderEvaluator();
+    		double userSimEvaluationScore = evaluator.evaluate(userSimRecBuilder,null,dataModel, 1, 0.5);
+    		System.out.println("Avaliação de Similaridade de Usuários : "+userSimEvaluationScore);
+    		
+    		
+    		/*
+	    	//Imprime usuários similares
+	    	for(LongPrimitiveIterator users=model.getUserIDs(); users.hasNext(); )
+	        {
+	            long userId = users.nextLong();
+	            long[] recommendedUserIDs = recommender.mostSimilarUserIDs(userId, 5); 
+	            
+	            for(long recID:recommendedUserIDs)
+	            {
+	                System.out.println("Usuário "+userId+" similar com Usuário "+recID +" similaridade de : "+similarity.userSimilarity(userId, recID));
+	            }
 
-
-	/* WANTED:
-	 * DataModel = { USER, ITEM, !PREFERENCE }
-	 * ! Foreach u' in USER, u' =  { ATRIBUTE(criteria)1.{estimativeVALUE1, estimativeVALUE2, ... },
-	 * 								 ATRIBUTE(criteria)2.{estimativeVALUE1, estimativeVALUE2, ... },
-	 * 								 ... }
-	 * 
-	 * EstimatedPreference is a estimated preference of i' for u' ,
-	 * where ePref(i', u') = 
-	 * 
-	 * MultiCriteriaRecommender recommends to an user u' a list of top items TI,
-	 * where i' in TI, ePref(i', u') and TI is ordered.
-	 * i.e.: returns items that have not been rated by the user, ordered by estimated preference of that user
-	 * */
-    
-    
+	        }	    	
+	    	//Especifica o Id do usuário e a qtd de itens a recomendar
+	    	List<RecommendedItem> recommendations = recommender.recommend(2, 5);	    	
+	    	for (RecommendedItem recommendationUser : recommendations) {
+	    	  System.out.println(recommendationUser);
+	    	}*/
+    		
+    	} catch (IOException e) {
+    		System.out.println("There was an IO exception.");
+			e.printStackTrace();
+    	} catch (TasteException e) {
+    		System.out.println("There was an Taste exception.");
+			e.printStackTrace();
+    	}
+    }    
 }
